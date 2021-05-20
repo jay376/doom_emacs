b7e052fd17af4cecaacd1dc0616f146a410ebd73
Fangjie Zhang <fangjie.zhang@shopee.com>
2021-05-20 15:35:04 +0800

    modify helm & grep

--- packages.el
+++ packages.el
@@ -13,6 +13,7 @@
 (package! grip-mode)
 (package! google-c-style)
 (package! protobuf-mode)
+(package! projectile-ripgrep)
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
