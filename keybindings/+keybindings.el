987d0f5abb969f586acfd471ca15a03a770b03df
Fangjie Zhang <fangjie.zhang@shopee.com>
2021-06-02 17:18:01 +0800

    modify grep

--- keybindings/+keybindings.el
+++ keybindings/+keybindings.el
@@ -32,7 +32,7 @@
        :desc "search-buffer"   "." #'swiper-isearch-thing-at-point
        )
 
-      (:desc "search-at-point" "/" #'+default/search-project-for-symbol-at-point)
+      (:desc "search-at-point" "/" #'helm-projectile-grep)
       )
 (map! :map org-mode-map
       "M-s RET"    #'org-insert-subheading
